08ff3c55db976f7197614d719a1b1713ebda84d0
Sebastian Willenborg <comawill@wlnbrg.de>
2014-09-30 17:09:16 +0200

    [examples/inga] aes: added an example how to use the aes-functionality

---
+++ examples/inga/aes/aes-example.c
@@ -0,0 +1,52 @@
+#include "contiki.h"
+#include <stdlib.h>
+#include <stdio.h> /* For printf() */
+#include "clock.h"
+#include <util/delay.h>
+#include <string.h>
+#include "dev/rf231/rf231.h"
+
+uint8_t key[AES_KEY_SIZE] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+uint8_t buffer[AES_BLOCK_SIZE];
+
+/*---------------------------------------------------------------------------*/
+void
+show_block(uint8_t *data)
+{
+  uint8_t i;
+  for(i = 0; i < AES_BLOCK_SIZE; i++) {
+    printf("%02x", data[i]);
+  }
+  printf("\n");
+}
+/*---------------------------------------------------------------------------*/
+PROCESS(test_process, "Test process");
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(test_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  printf("Set AES-Key: ");
+  show_block(key);
+  rf231_aes_128_set_key(key, 0);
+
+  memset(buffer, 0x12, AES_BLOCK_SIZE);
+  printf("Encrypt: ");
+  show_block(buffer);
+  rf231_aes_128_encrypt(buffer);
+  printf("Result: ");
+  show_block(buffer);
+
+  printf("Setup Decryption\n");
+  rf231_aes_128_set_key(key, 1);
+
+  printf("Decrypt: ");
+  show_block(buffer);
+  rf231_aes_128_decrypt(buffer);
+  printf("Result: ");
+  show_block(buffer);
+
+  PROCESS_END();
+}
+
+AUTOSTART_PROCESSES(&test_process);
